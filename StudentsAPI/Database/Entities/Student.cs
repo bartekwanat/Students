9b2c7a5be1eb011a23c20831ebf6b718bc2fa318
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-08-09 15:28:13 +0200

    Create student entity

--- StudentsAPI/Database/Entities/Student.cs
+++ StudentsAPI/Database/Entities/Student.cs
@@ -2,5 +2,9 @@
 {
     public class Student
     {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public DateOnly DateOfBirth { get; set; }
     }
 }
