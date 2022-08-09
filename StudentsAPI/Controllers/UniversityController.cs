3393c4fb2de2bd9a25a6cdc0752537b122da330f
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-08-09 21:58:38 +0200

    Add University Controllers

---
+++ StudentsAPI/Controllers/UniversityController.cs
@@ -0,0 +1,55 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using StudentsAPI.Models;
+using StudentsAPI.Services.Student.Query;
+using StudentsAPI.Services.University.Commands;
+using StudentsAPI.Services.University.Queries;
+
+namespace StudentsAPI.Controllers
+{
+    [ApiController]
+    [Route("api/University")]
+    public class UniversityController : ControllerBase
+    {
+        private IMediator _mediator;
+        protected IMediator Mediator => _mediator ??= HttpContext.RequestServices.GetService<IMediator>();
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll([FromQuery] PaginationModel pagination)
+        {
+            var result = await Mediator.Send(new GetAllUniversitiesQuery(){ Pagination = pagination });
+
+            return Ok(result);
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var result = await Mediator.Send(new GetUniversityQuery() { Id = id });
+
+            return Ok(result);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateUniversityCommand command)
+        {
+            return Ok(await Mediator.Send(command));
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            return Ok(await Mediator.Send(new DeleteUniversityCommand() { Id = id }));
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(Guid id, UpdateUniversityCommand command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest();
+            }
+
+            return Ok(await Mediator.Send(command));
+        }
+    }
+}
--- StudentsAPI/Services/University/Queries/GetAllUniversitiesQuery.cs
+++ StudentsAPI/Services/University/Queries/GetAllUniversitiesQuery.cs
@@ -2,11 +2,13 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
 using StudentsAPI.Database;
+using StudentsAPI.Models;
 
 namespace StudentsAPI.Services.University.Queries
 {
     public class GetAllUniversitiesQuery : IRequest<IEnumerable<Database.Entities.University>>
     {
+        public PaginationModel Pagination { get; set; }
         public class GetAllUniversitiesQueryHandler : IRequestHandler<GetAllUniversitiesQuery,
             IEnumerable<Database.Entities.University>>
         {
@@ -20,8 +22,9 @@ namespace StudentsAPI.Services.University.Queries
                 CancellationToken cancellationToken)
             {
                 var universitiesList = await _context.Universities
-                    .AsNoTracking()
-                    .ToListAsync(cancellationToken);
+                    .Skip(request.Pagination.ItemsPerPage * request.Pagination.page)
+                    .Take(request.Pagination.ItemsPerPage)
+                    .ToListAsync(cancellationToken: cancellationToken);
 
                 return universitiesList?.AsReadOnly();
             }
