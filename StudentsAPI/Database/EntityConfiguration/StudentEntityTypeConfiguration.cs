f45ebec5bdea5ae780a9cc6014003226214f2ca9
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-08-09 15:50:08 +0200

    Add UniversityEntityTypeConfiguration

---
+++ StudentsAPI/Database/EntityConfiguration/StudentEntityTypeConfiguration.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using StudentsAPI.Database.Entities;
+
+namespace StudentsAPI.Database.EntityConfiguration
+{
+    public class StudentEntityTypeConfiguration : IEntityTypeConfiguration<Student>
+    {
+        public void Configure(EntityTypeBuilder<Student> builder)
+        {
+            builder
+                .Property(x => x.FirstName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(x => x.LastName)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder
+                .Property(x => x.DateOfBirth)
+                .IsRequired()
+                .HasColumnType("date");
+
+            builder
+                .Property(x => x.UniversityId)
+                .IsRequired()
+                .HasColumnType("UNIQUEIDENTIFIER");
+        }
+    }
+}
