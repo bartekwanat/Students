c6bab513e1c82c57abdaca6fd006caf13d974f1b
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-08-09 15:36:37 +0200

    Create ApplicationDbContext

---
+++ StudentsAPI/Database/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StudentsAPI.Database.Entities;
+
+namespace StudentsAPI.Database
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Student> Students { get; set; }
+        public DbSet<University> Universities { get; set; }
+
+    }
+}
