41878192cf3b57d040d8c7f479cd166f530a01b9
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-08-09 21:45:03 +0200

    Add GetAllUniversitiesQuery

--- StudentsAPI/Services/University/Queries/GetAllUniversitiesQuery.cs
+++ StudentsAPI/Services/University/Queries/GetAllUniversitiesQuery.cs
@@ -1,6 +1,32 @@
-﻿namespace StudentsAPI.Services.University.Queries
+﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using StudentsAPI.Database;
+
+namespace StudentsAPI.Services.University.Queries
 {
-    public class GetAllUniversitiesQuery
+    public class GetAllUniversitiesQuery : IRequest<IEnumerable<Database.Entities.University>>
     {
+        public class GetAllUniversitiesQueryHandler : IRequestHandler<GetAllUniversitiesQuery,
+            IEnumerable<Database.Entities.University>>
+        {
+            private readonly IApplicationDbContext _context;
+            public GetAllUniversitiesQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<IEnumerable<Database.Entities.University>> Handle(GetAllUniversitiesQuery request,
+                CancellationToken cancellationToken)
+            {
+                var universitiesList = await _context.Universities
+                    .AsNoTracking()
+                    .ToListAsync(cancellationToken);
+
+                return universitiesList?.AsReadOnly();
+            }
+        }
+
     }
+
 }
