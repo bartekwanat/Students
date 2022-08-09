e52caf8bb0c5f00a142fd766e7c4e52ef9d30937
bartekwanat <61335440+bartekwanat@users.noreply.github.com>
2022-08-09 21:51:58 +0200

    Add GetUniversityQuery

---
+++ StudentsAPI/Services/University/Queries/GetUniversityQuery.cs
@@ -0,0 +1,31 @@
+﻿using System.Security.Cryptography.X509Certificates;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using StudentsAPI.Database;
+
+namespace StudentsAPI.Services.University.Queries
+{
+    public class GetUniversityQuery : IRequest<Database.Entities.University>
+    {
+        public Guid Id { get; set; }
+
+        public class GetUniversityQueryHandler : IRequestHandler<GetUniversityQuery, Database.Entities.University>
+        {
+            private readonly IApplicationDbContext _context;
+
+            public GetUniversityQueryHandler(IApplicationDbContext context)
+            {
+                _context = context;
+            }
+
+            public async Task<Database.Entities.University> Handle(GetUniversityQuery request, CancellationToken cancellationToken)
+            {
+                var university = _context.Universities
+                    .Include(x => x.Students)
+                    .FirstOrDefault(x => x.Id == request.Id);
+
+                return university;
+            }
+        }
+    }
+}
